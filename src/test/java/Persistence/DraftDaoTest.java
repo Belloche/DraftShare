852a30eea1cfdfde01e69a7ed8ac5821a1223f2d
Zane Miller <ztmiller2@madisoncollege.edu>
2021-03-03 14:37:15 -0600

    All tests for Draft entity and DAO are passing, ready for Checkpoint 2

--- src/test/java/Persistence/DraftDaoTest.java
+++ src/test/java/Persistence/DraftDaoTest.java
@@ -26,4 +26,35 @@ public class DraftDaoTest {
         assertEquals(1, retrievedDraft.getId());
         assertEquals("testDraft", retrievedDraft.getDraftName());
     }
+    
+    @Test
+    void insertSuccess() {
+        Draft newDraft = new Draft("Evan", "testDraft", "this is a test", 5, 0,"Jaguars", "Trevor Lawrence", "QB", "Clemson","Jets", "Justin Fields", "QB", "Ohio State","Dolphins", "Penei Sowell", "OT", "Oregon","Jaguars", "Trevor Lawrence", "QB", "Clemson","Jaguars", "Trevor Lawrence", "QB", "Clemson","Jaguars", "Trevor Lawrence", "QB", "Clemson","Jaguars", "Trevor Lawrence", "QB", "Clemson","Jaguars", "Trevor Lawrence", "QB", "Clemson","Jaguars", "Trevor Lawrence", "QB", "Clemson","Jaguars", "Trevor Lawrence", "QB", "Clemson","Jaguars", "Trevor Lawrence", "QB", "Clemson","Jaguars", "Trevor Lawrence", "QB", "Clemson","Jaguars", "Trevor Lawrence", "QB", "Clemson","Jaguars", "Trevor Lawrence", "QB", "Clemson","Jaguars", "Trevor Lawrence", "QB", "Clemson","Jaguars", "Trevor Lawrence", "QB", "Clemson","Jaguars", "Trevor Lawrence", "QB", "Clemson","Jaguars", "Trevor Lawrence", "QB", "Clemson","Jaguars", "Trevor Lawrence", "QB", "Clemson","Jaguars", "Trevor Lawrence", "QB", "Clemson","Jaguars", "Trevor Lawrence", "QB", "Clemson","Jaguars", "Trevor Lawrence", "QB", "Clemson","Jaguars", "Trevor Lawrence", "QB", "Clemson","Jaguars", "Trevor Lawrence", "QB", "Clemson","Jaguars", "Trevor Lawrence", "QB", "Clemson","Jaguars", "Trevor Lawrence", "QB", "Clemson","Jaguars", "Trevor Lawrence", "QB", "Clemson", "Jaguars", "Trevor Lawrence", "QB", "Clemson", "Jaguars", "Trevor Lawrence", "QB", "Clemson", "Jaguars", "Trevor Lawrence", "QB", "Clemson", "Jaguars", "Trevor Lawrence", "QB", "Clemson", "Jaguars", "Trevor Lawrence", "QB", "Clemson");
+        int id = dao.insert(newDraft);
+        assertNotEquals(0, id);
+        Draft insertedDraft = dao.getById(id);
+        assertEquals("Evan", insertedDraft.getUser());
+    }
+
+    @Test
+    void updateSuccess() {
+        String newPick = "Brett Favre";
+        Draft draftToUpdate = dao.getById(1);
+        draftToUpdate.setPick1(newPick);
+        dao.saveOrUpdate(draftToUpdate);
+        Draft draftAfterUpdate = dao.getById(1);
+        assertEquals(newPick, draftAfterUpdate.getPick1());
+    }
+
+    @Test
+    void deleteSuccess() {
+        dao.delete(dao.getById(2));
+        assertNull(dao.getById(2));
+    }
+
+    @Test
+    void getAllSuccess() {
+        List<Draft> Drafts = dao.getAll();
+        assertEquals(2, Drafts.size());
+    }
 }
